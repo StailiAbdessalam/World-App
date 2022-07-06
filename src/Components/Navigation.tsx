884efce22f934256f1685041f7d9241497f75bf1
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-06 14:43:28 +0100

    make components navigation

---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <NavLink exact to="/" activeClassName="nav-action">
+        Accueil
+      </NavLink>
+      <NavLink exact to="a-propos" activeClassName="nav-action">
+        A Propos
+      </NavLink>
+    </div>
+  );
+}; 
+
+export default Navigation;
