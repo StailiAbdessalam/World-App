93725cd1b4f656cadb74f8bf88d57f602cb149c8
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-06 14:44:04 +0100

    page a propos

---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>A propos</h1>
+        <br />
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magni minus molestiae rem? Eligendi, eos. Unde, voluptatibus saepe quod nam totam ipsam nesciunt quisquam repellat eaque dicta odio quae praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse incidunt dolorem dolor quos quibusdam. Nihil quia id vero, molestias aliquam at accusantium perferendis? Magni consequatur, adipisci facere, nostrum, incidunt suscipit itaque ad explicabo asperiores quaerat labore rerum provident totam dicta accusamus necessitatibus voluptatum placeat. Fugiat facilis quibusdam nesciunt ipsam at excepturi, nisi qui hic? Error repellendus placeat earum. Deleniti reprehenderit at, excepturi ullam dicta, natus quis voluptas alias repellat molestiae sit reiciendis assumenda voluptatibus iure dolore accusantium velit nihil! Exercitationem laborum omnis voluptas provident ullam repudiandae fugit sapiente impedit inventore. Quidem provident vero ab praesentium nam cumque sint! Quo.
+        </p>
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque iure eos ut pariatur quam earum asperiores, omnis porro temporibus labore iusto quod optio error animi impedit minus aspernatur autem cupiditate voluptas? Accusamus provident dolor quam minus ipsa cupiditate fugiat id obcaecati ullam doloribus fugit, eaque quisquam facilis repellendus ea aperiam omnis hic architecto adipisci! Obcaecati nobis inventore reiciendis, odit saepe deleniti optio, necessitatibus iste, totam dicta perspiciatis neque? Necessitatibus soluta fugit placeat magni cupiditate modi accusamus praesentium quibusdam odit, nostrum fuga blanditiis cum veritatis eius laborum sed repudiandae ut quo, asperiores cumque aut quos tempore repellendus. Aliquam molestiae illum quaerat minus. Ducimus molestiae dignissimos eaque, incidunt harum non magni vel nulla repellendus minus beatae veniam, accusamus atque modi facere tempora quibusdam cum. Aliquam, ex nihil fuga officia exercitationem necessitatibus saepe facere, repellendus nam in, atque quos eligendi. Quibusdam, quis ducimus?</p>
+    </div>
+  )
+}
+
+export default About
