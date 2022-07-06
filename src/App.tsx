6e2d6c8a8feacd525105f9384017c21de578ee85
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-06 14:42:18 +0100

    make router in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import NotFound from "../src/pages/NotFound";
+import About from "../src/pages/About";
+import Layout from "./layout/layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Layout>
+            <Home />
+          </Layout>
+        </Route>
+        <Route path="/a-propos" exact >
+          <Layout>
+            <About />
+          </Layout>
+        </Route>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
