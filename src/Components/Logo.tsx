74118336a727e5993baa6c6a4de4e2790c912697
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-06 14:43:14 +0100

    make compoents logo de web application

---
+++ src/Components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+        <img src={require('../assets/download-removebg-preview.png')} alt="" />
+      <h3>BENY33X</h3>
+    </div>
+  );
+};
+export default Logo;
