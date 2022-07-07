f2add5624cefb38f5ae4ac7667c27270f091f98d
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-07 14:53:39 +0100

    components contries

--- src/Components/Contries.tsx
+++ src/Components/Contries.tsx
@@ -1,20 +1,24 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 const Contries = () => {
   const [data, setData]: any = useState([]);
-  
-  axios.get('https://restcountries.com/v3.1/all').then((res)=>{
-    setData(res.data);
-    
-  })
-  console.log(data);
-  
-  return (
-    <div>
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      setData(res.data);
+    });
+  }, []);
 
+  return (
+    <div className="countries">
+      <ul className="countries-list">
+        {data.map((country:any)=>(
+         <li>{country.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 };
 
 export default Contries;
